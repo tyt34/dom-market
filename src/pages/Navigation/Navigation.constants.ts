7204010792ed81181c5be30330aa79b0873f1c40
novoto4in <novoto4in@mail.ru>
2026-05-06 17:19:44 +0300

    удалил неиспользуемый файл

--- src/pages/Navigation/Navigation.constants.ts
+++
@@ -1,9 +0,0 @@
-import { pages } from '../../app/config-pages'
-
-export const navigateConfig = [
-  {
-    href: `${pages.map.pathForWatch}1`,
-    navigate: pages.map.path,
-    title: 'Example Map 1',
-  },
-]
