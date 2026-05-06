626eaa8df764922ae68328d860ac2128765f63f5
novoto4in <novoto4in@mail.ru>
2026-05-06 13:08:22 +0300

    стилизовал footer-bottom под desktop

--- src/widgets/Footer/components/FooterBottom/FooterBottom.tsx
+++ src/widgets/Footer/components/FooterBottom/FooterBottom.tsx
@@ -3,19 +3,29 @@ import styles from './FooterBottom.module.scss'
 export const FooterBottom = () => {
   return (
     <div className={`${styles.wrapper} white`}>
-      <div className={`${styles.top} w-500 t-9`}>
+      <div className={`${styles.top} w-500 t-9-17`}>
         <p>Ждём вас в гости!</p>
         <p>© Живём дома, 2012 – {new Date().getFullYear()}</p>
       </div>
 
-      <p className="t-7 mt-[7]">
-        Информация, указанная на данном сайте, носит ознакомительный
-        характер и не является публичной офертой
-      </p>
+      <div className={styles.right}>
+        <p className="t-7 mt-[7]">
+          Информация, указанная на данном сайте, носит ознакомительный
+          характер и не является публичной офертой
+        </p>
 
-      <p className="t-7 mt-[2]">
-        Сайт работает при поддержке студии ClickON
-      </p>
+        <div className={styles.desktop}>
+          <p className="t-7">
+            Сайт работает при поддержке студии ClickON
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.mobile}>
+        <p className="t-7 mt-[2]">
+          Сайт работает при поддержке студии ClickON
+        </p>
+      </div>
     </div>
   )
 }
